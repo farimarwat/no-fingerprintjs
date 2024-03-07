9994b7fce2db9b3547cb2998d16cc9434cd6801f
Farman Ullah Khan <farimarwat@gmail.com>
2024-03-07 10:16:40 +0500

    Improve WebGl Randomization

--- no-fingerprintjs.js
+++ no-fingerprintjs.js
@@ -2,8 +2,8 @@
 //@script-version: 1.0
 
 //uncomment for extension
-let script = document.createElement("script");
-script.textContent = "(" + (function () {
+// let script = document.createElement("script");
+// script.textContent = "(" + (function () {
 
 	"use strict";
 	const KEY_CANVAS_HEIGHT = "canvasHeight";
@@ -394,5 +394,5 @@ script.textContent = "(" + (function () {
 	})();
 
 	//uncomment for extension
-}) + ")()";
-document.documentElement.prepend(script);
+// }) + ")()";
+// document.documentElement.prepend(script);
